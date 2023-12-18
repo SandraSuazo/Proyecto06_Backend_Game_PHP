729cd4dd210252483a3669c8901537cacfe0756e
Sandra <sandrasuazo96@gmail.com>
2023-12-18 22:56:51 +0100

    feat: create MessageController file

--- routes/api.php
+++ routes/api.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\AuthController;
+use App\Http\Controllers\MessageController;
 use App\Http\Controllers\RoomController;
 use App\Http\Controllers\UserController;
 use Illuminate\Http\Request;
@@ -14,14 +15,21 @@ Route::get('/healthcheck', function (Request $request) {
 Route::post('/login', [AuthController::class, 'login']);
 Route::post('/logout', [AuthController::class, 'logout'])->middleware('auth:sanctum');
 
-//USER
+//USERS
 Route::post('/register', [UserController::class, 'register']);
 Route::get('/profile', [UserController::class, 'profile']);
 Route::put('/user', [UserController::class, 'updateProfile']);
 Route::delete('/user', [UserController::class, 'deleteUser']);
 
-//ROOM
+//ROOMS
 Route::post('/room', [RoomController::class, 'createRoom']);
 Route::get('/room', [RoomController::class, 'getRoomById']);
 Route::put('/room', [RoomController::class, 'updateRoom']);
 Route::delete('/delete', [RoomController::class, 'deleteRoom']);
+
+// MESSAGES
+Route::post('/message', [MessageController::class, 'createMessage']);
+Route::get('/message', [MessageController::class, 'getMessageById']);
+Route::get('/messages', [MessageController::class, 'getAllRoomMessage']);
+Route::put('/message', [MessageController::class, 'updatedMessage']);
+Route::delete('/message', [MessageController::class, 'deleteMessage']);
