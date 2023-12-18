5d466ab8b29ff91422917bfc85010c7fa57b6d80
Sandra <sandrasuazo96@gmail.com>
2023-12-18 19:24:16 +0100

    fix: route logout

--- routes/api.php
+++ routes/api.php
@@ -21,4 +21,4 @@ Route::get('/healthcheck', function (Request $request) {
 
 Route::post('/register', [AuthController::class, 'register']);
 Route::post('/login', [AuthController::class, 'login']);
-Route::post('/logout', [AuthController::class, 'logout']);
+Route::post('/logout', [AuthController::class, 'logout'])->middleware('auth:sanctum');
